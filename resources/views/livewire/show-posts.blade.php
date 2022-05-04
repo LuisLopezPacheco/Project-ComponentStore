35dc924e3f881eed768a15d2697cb871b220f5f6
Luis Fernando López Pacheco <luis.lopez0413@alumnos.udg.mx>
2022-05-03 23:05:47 -0500

    Creación de slot y primeras pruebas con tabla componentes

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -6,5 +6,35 @@
         </h2>
     </x-slot>
     <h1>Hola amigos</h1>
-    {{$posts}}
+    <x-table>
+        <p class="text-lg text-center font-bold m-5">Gradient Table Design</p>
+        <table class="rounded-t-lg m-5 w-5/6 mx-auto text-gray-100 bg-gradient-to-l from-indigo-500 to-indigo-800">
+            <tr class="text-left border-b-2 border-indigo-300">
+                <th class="px-4 py-3">Firstname</th>
+                <th class="px-4 py-3">Lastname</th>
+                <th class="px-4 py-3">Age</th>
+                <th class="px-4 py-3">Sex</th>
+            </tr>
+            <tr class="border-b border-indigo-400">
+                <td class="px-4 py-3">Jill</td>
+                <td class="px-4 py-3">Smith</td>
+                <td class="px-4 py-3">50</td>
+                <td class="px-4 py-3">Male</td>
+            </tr>
+            <!-- each row -->
+            <tr class="border-b border-indigo-400">
+                <td class="px-4 py-3">Jill</td>
+                <td class="px-4 py-3">Smith</td>
+                <td class="px-4 py-3">50</td>
+                <td class="px-4 py-3">Male</td>
+            </tr>
+            <!-- each row -->
+            <tr class="border-b border-indigo-400">
+                <td class="px-4 py-3">Jill</td>
+                <td class="px-4 py-3">Smith</td>
+                <td class="px-4 py-3">50</td>
+                <td class="px-4 py-3">Male</td>
+            </tr>
+            <!-- each row -->
+    </x-table>
 </div>
