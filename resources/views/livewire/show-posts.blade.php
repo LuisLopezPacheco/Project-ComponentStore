91d9de720f07b8cbcf9f95b9685d8ef97e465843
Luis López <luis.lopez0413@alumnos.udg.mx>
2022-05-03 19:22:32 -0500

    Primeras pruebas

---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,10 @@
+<div>
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <h1>Hola amigos</h1>
+    
+</div>
